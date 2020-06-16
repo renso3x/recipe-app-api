b64f41a9bee54ad0bd4b5e7f15a3af4efa76c003
renso3x <enso123456@gmail.com>
2020-06-16 17:29:29 +0800

    update file for flake8

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -34,8 +34,8 @@ class AdminSiteTests(TestCase):
         self.assertEqual(res.status_code, 200)
 
     def test_create_user_page(self):
-      """ Test that the create user page works """
-      url = reverse('admin:core_user_add')
-      res = self.client.get(url)
+        """ Test that the create user page works """
+        url = reverse('admin:core_user_add')
+        res = self.client.get(url)
 
-      self.assertEqual(res.status_code, 200)
+        self.assertEqual(res.status_code, 200)
